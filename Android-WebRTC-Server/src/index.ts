458edaee49bbb577ef33af3335af1749caf0c64b
DDPlay123 <a70325000@gmail.com>
2023-05-19 00:12:26 +0800

    feat: build WebSoucket server

--- Android-WebRTC-Server/src/index.ts
+++ Android-WebRTC-Server/src/index.ts
@@ -4,7 +4,7 @@ import Room from "./room";
 const mWs = new WebSocketServer({ port: 8000 });
 const mRoom = new Room();
 
-console.log("Server started on port 8080");
+console.log("Server started on port 8000");
 console.log(mWs.address());
 
 mWs.on("connection", (ws) => {
